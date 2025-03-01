418f40141cf7f16a16e6d27c22138f9ed2abfc85
Cfathurrohman <cfathurrohman@gmail.com>
2025-03-01 22:08:27 +0700

    Initial Commit

---
+++ my-app/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Selamat Datang di Website Saya!</h1>
+      <p>Ini adalah halaman utama.</p>
+      <a href="/about">
+        Tentang Kami
+      </a>
+    </div>
+  )
+}
+
+export default HomePage;
