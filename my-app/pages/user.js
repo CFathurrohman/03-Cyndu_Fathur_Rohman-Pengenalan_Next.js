83d17bb8ef47d4b728308db53e9ac6a6aeb88982
Cfathurrohman <cfathurrohman@gmail.com>
2025-03-04 17:01:51 +0700

    Add task 1 "display list of users"

---
+++ my-app/pages/user.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import Link from "next/link";
+
+const UserList = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await fetch("/api/user");
+      const users = await response.json();
+      setUsers(users);
+    };
+    fetchUsers();
+  }, []);
+
+  return (
+    <div>
+      <h1>Daftar User</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <Link href={`/users/${user.id}`}>{user.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UserList;
